1997ba0ea096bc7f40fc00e12050183b1501eb0f
Dário Pereira <pereira13.dario@gmail.com>
2019-10-09 15:34:05 +0100

    Classe Comida

--- Snake2/src/iul/ista/pi/snake/Comida.java
+++ Snake2/src/iul/ista/pi/snake/Comida.java
@@ -1,15 +1,41 @@
 package iul.ista.pi.snake;
 
-import java.awt.Image;
-
 public class Comida {
-	
+
 	private Food_Type type;
-	
-	private Image icon;
-	private double [][] pontuacao = { {}, {}, {}, {}};
+
 	private int x;
 	private int y;
-	
 
+	public Comida() {
+		genaratePosition();
+		generateType();
+	}
+
+	private void generateType() {
+		type = Food_Type.CHERRY;
+		// TODO Auto-generated method stub
+
+	}
+
+	public void genaratePosition() {
+		int r = (int) (Math.random() * Board.RAND_POS);
+		x = ((r * Board.DOT_SIZE));
+
+		r = (int) (Math.random() * Board.RAND_POS);
+		y = ((r * Board.DOT_SIZE));
+
+	}
+	
+	public int getX() {
+		return x;
+	}
+
+	public int getY() {
+		return y;
+	}
+	
+	public Food_Type getType() {
+		return type;
+	}
 }
