56a9cfb8d44297ff2b09168865e24a61f5d4eb29
Dário Pereira <pereira13.dario@gmail.com>
2019-10-16 23:35:01 +0100

    Diversas alteracoes

--- Snake2/src/iul/ista/pi/snake/Enemy.java
+++ Snake2/src/iul/ista/pi/snake/Enemy.java
@@ -5,7 +5,62 @@ import java.awt.Image;
 import javax.swing.ImageIcon;
 
 public class Enemy implements Colisivel {
-	
+
+	private static final int DESLOCAMENTO = 20;
+	private static final int NUM_MOVIMENTOS = 4;
+
 	private Image img = new ImageIcon("resources/enemy.png").getImage();
+
+	private int x;
+	private int y;
+	private boolean move_x_cima = false;
+	private boolean move_y_esquerda = false;
+	private boolean move_x_baixo = true;
+	private boolean move_y_direita = true;
+	private int contador_direcoes;
+	private int contador_movimentos;
+
+	public Enemy(Comida c) {
+		this.x = c.getX() - DESLOCAMENTO;
+		this.y = c.getY() - DESLOCAMENTO;
+	}
+
+	public void move() {
+		if (contador_movimentos == 4) {
+			contador_movimentos=0;
+			if ((contador_direcoes + 1) == 4)
+				contador_direcoes = 0;
+			else
+				contador_direcoes++;
+		}
+		contador_movimentos++;
+		Direcao direcao = Direcao.values()[contador_direcoes];
+		switch (direcao) {
+		case BAIXO:
+			this.x+=10;
+			break;
+		case DIREITA:
+			this.y+=10;
+			break;
+		case CIMA:
+			this.x-=10;
+			break;
+		default:
+			this.y-=10;
+			break;
+		}
+	}
+	
+	public Image getImg() {
+		return img;
+	}
+	
+	public int getX() {
+		return x;
+	}
+	
+	public int getY() {
+		return y;
+	}
 
 }
--- Snake2/src/iul/ista/pi/snake/Snake.java
+++ Snake2/src/iul/ista/pi/snake/Snake.java
@@ -1,6 +1,15 @@
 package iul.ista.pi.snake;
 import java.awt.EventQueue;
+
 import javax.swing.JFrame;
+import java.awt.BorderLayout;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class Snake extends JFrame {
 
@@ -8,15 +17,47 @@ public class Snake extends JFrame {
 	 * 
 	 */
 	private static final long serialVersionUID = 4304994289171574723L;
+	private static Snake INSTANCE;
+	private Board board;
+	private JLabel pontos, temperatura;
 
-	public Snake() {
+	private Snake() {
         
         initUI();
     }
     
     private void initUI() {
+    	
+    	board = new Board();
+    	
+        getContentPane().setLayout(new BorderLayout(0, 0));
         
-        add(new Board());
+        getContentPane().add(board);
+        
+        JPanel panel_info = new JPanel();
+        getContentPane().add(panel_info, BorderLayout.NORTH);
+        panel_info.setLayout(new BorderLayout(0, 0));
+        
+        JPanel panel_pontuacao = new JPanel();
+        panel_info.add(panel_pontuacao, BorderLayout.WEST);
+        
+        JLabel lblPontuao = new JLabel("Pontua\u00E7\u00E3o: ");
+        panel_pontuacao.add(lblPontuao);
+        
+        pontos = new JLabel("0000");
+        panel_pontuacao.add(pontos);
+        
+        JPanel panel_temperatura = new JPanel();
+        panel_info.add(panel_temperatura, BorderLayout.EAST);
+        
+        JLabel lblTemperatura = new JLabel("Temperatura:");
+        panel_temperatura.add(lblTemperatura);
+        
+        temperatura = new JLabel("00");
+        panel_temperatura.add(temperatura);
+        
+        JLabel lblc = new JLabel("\u00BAC");
+        panel_temperatura.add(lblc);
         
         setResizable(false);
         pack();
@@ -24,6 +65,52 @@ public class Snake extends JFrame {
         setTitle("Snake");
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        JMenuBar menuBar = new JMenuBar();
+        setJMenuBar(menuBar);
+        
+        JMenu mnMenu = new JMenu("Menu");
+        menuBar.add(mnMenu);
+        
+        JMenuItem mntmOpes = new JMenuItem("Op\u00E7\u00F5es");
+        mnMenu.add(mntmOpes);
+        
+        JMenuItem mntmReiniciar = new JMenuItem("Reiniciar");
+        mnMenu.add(mntmReiniciar);
+        
+        JMenuItem mntmSair = new JMenuItem("Sair");
+        mnMenu.add(mntmSair);
+        
+        JMenu mnAjuda = new JMenu("Ajuda");
+        menuBar.add(mnAjuda);
+        
+        JMenuItem m_regras = new JMenuItem("Regras");
+        mnAjuda.add(m_regras);
+        
+        JMenuItem mntmSobre = new JMenuItem("Sobre");
+        mntmSobre.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent arg0) {
+        		board.getTimer().stop();
+        		new Sobre(Snake.this).open();
+        		board.getTimer().start();
+        	}
+        });
+        mnAjuda.add(mntmSobre);
+    }
+    
+    public JLabel getPontos() {
+		return pontos;
+	}
+    
+    public JLabel getTemperatura() {
+		return temperatura;
+	}
+    
+    public static Snake getInstance(){
+    	if(INSTANCE==null){
+    		INSTANCE=new Snake();
+    	}
+    	return INSTANCE;
     }
     
     public static void main(String[] args) {
