49986a18e73dd94088328acddbf65f3e528e69ed
rmdca-iscteiul <rmdca@iscte-iul.pt>
2019-10-23 10:53:14 +0100

    Implementação da função para gerar a posição das paredes
    nota:Falta só resolver um problema com a sobreposição das paredes.

--- Snake2/src/iul/ista/pi/snake/Parede.java
+++ Snake2/src/iul/ista/pi/snake/Parede.java
@@ -11,6 +11,14 @@ public class Parede {
 	private int x;
 	private int y;
 
+	/*
+	 * 1/125 *( 0 + (int) (Math.random() * ((50 - 0) + 1)) 1/1500 * ( 50 + (int)
+	 * (Math.random() * ((350 - 50) + 1))) 1/125 * ( 350 + (int) (Math.random() *
+	 * ((400 - 350) + 1)))
+	 * 
+	 * 
+	 */
+
 	public Parede() {
 		generatePosition();
 	}
@@ -25,11 +33,23 @@ public class Parede {
 	}
 
 	public void generatePosition() {
-		int r = (int) (Math.random() * Board.RAND_POS);
-		x = ((r * Board.DOT_SIZE));
+//		int r = (int) (Math.random() * Board.RAND_POS);
+//		x = ((r * Board.DOT_SIZE));
+		double a = 0.4 * (0 + (int) (Math.random() * ((50 - 0) + 1)))
+				+ 0.2 * 1 / 1500 * (50 + (int) (Math.random() * ((350 - 50) + 1)))
+				+ 0.4 * (350 + (int) (Math.random() * ((400 - 350) + 1)));
+		
+		x = (int) (a+0.5);
+		//1/125 1/1500 1/125      1/125 * (u(x) - u(x-50)) + 1/1500 * (u(x-50)-u(x-350)) + 1/125 * (u(x-350) - u(x-400))
+		a = 0.4 * (0 + (int) (Math.random() * ((50 - 0) + 1)))
+				+ 0.2 * 1 / 1500 * (50 + (int) (Math.random() * ((350 - 50) + 1)))
+				+ 0.4 * (350 + (int) (Math.random() * ((400 - 350) + 1)));
+		
+		y = (int) (a+0.5);
+		
 
-		r = (int) (Math.random() * Board.RAND_POS);
-		y = ((r * Board.DOT_SIZE));
+//		r = (int) (Math.random() * Board.RAND_POS);
+//		y = ((r * Board.DOT_SIZE));
 
 	}
 
